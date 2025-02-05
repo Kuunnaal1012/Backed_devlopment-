6ec561f304f23031ee6c33583ad4f27a9da8a985
Kunal Khandale <90026353+Kuunnaal1012@users.noreply.github.com>
2025-02-05 11:06:21 +0530

    app.js

--- src/app.js
+++ src/app.js
@@ -1,27 +1,42 @@
-import express from 'express';
-import cors from 'cors'; // For handling CORS (Cross-Origin Resource Sharing)
-import cookieParser from 'cookie-parser'; // For parsing cookies
-const app = express();
+import express from 'express'
+import cors from 'cors' // front-end aur backed ki url link karne ke liye use karte hai issko 
+import cookieParser from 'cookie-parser'  /// user ke browser mein data data access aur install karne liye 
+const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN, // Set allowed origins dynamically
-    credentials: true,  // Allow credentials like cookies
-}));
+    origin: process.env.CORS_ORIGIN, // ek front ki link jaah se data aayega
+    credential: true,
+}))
+//middleware 
+app.use(express.json({limit:"16kb"})) // data ki max limit set kari hai json format mein 
+app.use(express.urlencoded({extended:true, limit:"16kb"}))  // url ke liye use karte hai jab encoded form mein rehti hai 
+app.use(express.static("public"))  // public folder , static file access karega 
+app.use(cookieParser())  /// user ke browser mein cookies store aur access karne ke liye 
 
-// Middleware for parsing JSON and URL-encoded data
-app.use(express.json({ limit: '16kb' })); // Limit body size for JSON requests
-app.use(express.urlencoded({ extended: true, limit: '16kb' })); // Limit body size for URL-encoded form data
 
-// Middleware for serving static files (e.g., images, CSS)
-app.use(express.static('public'));
 
-// Middleware for parsing cookies
-app.use(cookieParser());
 
-// Import user routes
-import userRouter from './routes/user.routes.js';
+import userRouter from './routes/user.routes.js'
 
-// Routes
-app.use('/api/v1/user', userRouter);
 
-export { app };
+// routes declaration 
+
+app.use("/api/v1/user", userRouter)
+
+
+export {app}
+
+
+                                        /*  charcha on middile-ware 
+
+1)  when  client hits  url 
+    (req, res)=>{
+        then(
+        resolve wala part          // to create a check point for a condition before getting data to user            
+        )                             just like , insta account check karne se pehele woh login karne ke liye check karta hai
+        catch()                       /// it can be more then one 
+}                              like 
+                              login hai toh two factor hai fir usska pop up lagega 
+
+
+*/
